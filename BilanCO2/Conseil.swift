ea02add226fa5f6996621bc379abd6ee28a8f5f2
jeromekasparian <jk@kasparian.eu>
2022-09-17 22:39:11 +0200

    Commit des fichiers qui ne sont pas passés au premier tour.

---
+++ BilanCO2/Conseil.swift
@@ -0,0 +1,29 @@
+//
+//  Conseil.swift
+//  BilanCO2
+//
+//  Created by Jérôme Kasparian on 08/09/2022.
+//
+
+import Foundation
+import UIKit
+
+//protocol ConseilDelegate: AnyObject {
+//    func ecritMessage(view: Conseil, texte: String)
+//}
+
+class Conseil: UIViewController {
+//    @IBOutlet var labelConseilPopover: UILabel!
+    
+//    weak var delegate: ConseilDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        labelConseilPopover.text = "Toto"
+    }
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//    }
+}
